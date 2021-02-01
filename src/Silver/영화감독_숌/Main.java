ad33f7ac047ff3681aa2d6c1f2515be8efeb5c20
JayAnCode <eyerun95@ajou.ac.kr>
2021-02-01 13:21:29 +0900

    [Sliver5]feat: 영화감독 숍

--- src/Silver/영화감독_숌/Main.java
+++ src/Silver/영화감독_숌/Main.java
@@ -6,10 +6,10 @@ import java.util.Stack;
 public class Main {
 	public static void main(String args[]) throws IOException {
 
+		Stack<String> stack = new Stack<String>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int count = 666;
-		Stack<String> stack = new Stack<>();
 		while(stack.size()<n){
 			String s = Integer.toString(count);
 			if(s.contains("666")) stack.push(s);
