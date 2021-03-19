6a1b875d542653624e84f4ddb13e59d99574e5ee
JayAnCode <eyerun95@ajou.ac.kr>
2021-03-19 16:19:10 +0900

    [Sliver3]feat: 이친수

---
+++ src/Silver/이친수/Main.java
@@ -0,0 +1,19 @@
+package Silver.이친수;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String arg[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int dp[][] = new int[N+1][2];
+		dp[1][0] = 0;
+		dp[1][1] = 1;
+		for(int i=2; i<N+1; i++) {
+			dp[i][0] = dp[i-1][0] + dp[i-1][1];
+			dp[i][1] = dp[i-1][0];
+		}
+		System.out.println(dp[N][0]+dp[N][1]);
+		br.close();
+	}
+}
