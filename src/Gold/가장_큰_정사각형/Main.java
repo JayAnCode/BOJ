aadf8a0951d24a048969f83560f762a0537093d6
JayAnCode <eyerun95@ajou.ac.kr>
2021-05-26 11:37:58 +0900

    [Gold5]feat: 가장 큰 정사각형

---
+++ src/Gold/가장_큰_정사각형/Main.java
@@ -0,0 +1,37 @@
+package Gold.가장_큰_정사각형;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String arg[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int map[][] = new int[n][m];
+		int dp[][] = new int[n][m];
+		for(int i=0; i<n; i++) {
+			String s = br.readLine();
+			for(int j=0; j<m; j++) {
+				map[i][j] = s.charAt(j)-'0';
+				dp[i][j] = map[i][j];
+			}
+		}
+
+		int max = 0;
+		for(int i=1; i<n; i++) {
+			for(int j=1; j<m; j++) {
+				if(map[i][j] == 1) {
+					dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j])) + 1;
+					max = Math.max(max, dp[i][j]);
+				}		// i,j의 왼쪽, 위쪽, 대각선 중 가장 작은 값 + 1이 정사각형을 이루는 한변의 길이, 가장 큰 값을 max에 저장
+			}
+		}
+
+		System.out.println((int)Math.pow(max, 2));
+		br.close();
+	}
+}
