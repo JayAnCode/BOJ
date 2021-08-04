c20026d622cd95306b5277230837b0fb0ca281c2
JayAnCode <eyerun95@ajou.ac.kr>
2021-08-04 09:25:29 +0900

    [Gold5]refactor: 연구소

--- src/Gold/연구소/Main.java
+++ src/Gold/연구소/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	static boolean check[][];
 	static int dx[] = {-1, 1, 0, 0};
 	static int dy[] = {0, 0, -1, 1};
+
 	static class Pair {
 		int row;
 		int col;
@@ -20,9 +21,11 @@ public class Main {
 			this.col = col;
 		}
 	}
+
 	public static void main(String arg[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
@@ -36,13 +39,15 @@ public class Main {
 
 		ans = Integer.MIN_VALUE;
 		go(0);
+
 		System.out.println(ans);
 		br.close();
 	}
 	public static void go(int count) {
-		if(count == 3) {
-			copy();
+		if(count == 3) {	// 벽이 3개 일 때 최대 영역의 크기 찾기 시작
+			copy();	// 벽이 3개 일 때마다 영역의 크기를 계산하기 위해 map을 복사
 			check = new boolean[N][M];
+
 			for(int i=0; i<N; i++) {
 				for (int j = 0; j < M; j++) {
 					if(copyMap[i][j] == 2 && !check[i][j]) {
@@ -50,29 +55,36 @@ public class Main {
 					}
 				}
 			}
-			int cur = calc();
+
+			int cur = calc();	// 영역의 크기 계산
+
 			ans = Math.max(ans, cur);
 			return;
 		}
+
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
 				if(map[i][j] == 0) {
-					map[i][j] = 1;
+					map[i][j] = 1;	// 벽을 새로 세움
 					go(count+1);
 					map[i][j] = 0;
 				}
 			}
 		}
 	}
+
 	public static void bfs(int row, int col) {
 		Queue<Pair> q = new LinkedList<>();
 		q.offer(new Pair(row, col));
 		check[row][col] = true;
+
 		while(!q.isEmpty()) {
 			Pair p = q.poll();
+
 			for(int i=0; i<4; i++) {
 				int nx = p.col + dx[i];
 				int ny = p.row + dy[i];
+
 				if((nx>=0 && nx<M) && (ny>=0 && ny<N)) {
 					if(copyMap[ny][nx] == 0) {
 						copyMap[ny][nx] = 2;
@@ -81,8 +93,9 @@ public class Main {
 					}
 				}
 			}
-		}
+		}	// 바이러스가 퍼지는 과정
 	}
+
 	public static void copy() {
 		copyMap = new int[N][M];
 		for(int i=0; i<N; i++) {
@@ -91,13 +104,14 @@ public class Main {
 			}
 		}
 	}
+
 	public static int calc() {
 		int count = 0;
 		for(int i=0; i<N; i++) {
 			for (int j=0; j<M; j++) {
 				if(copyMap[i][j] == 0) count++;
 			}
-		}
+		}	// 안전 영역 크기 계산
 		return count;
 	}
 }
