805a3cd22ff1b0bb203fdfedf5af1a9aa6ca9383
JayAnCode <eyerun95@ajou.ac.kr>
2021-08-08 14:10:51 +0900

    [Gold5]refactor: N번째 큰 수

--- src/Gold/N번째_큰_수/Main.java
+++ src/Gold/N번째_큰_수/Main.java
@@ -5,16 +5,27 @@ import java.util.*;
 
 public class Main {
 	public static void main(String arg[]) throws IOException {
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());	// 우선순위 큐 역순으로 정렬(내림차순)
+
 		int N = Integer.parseInt(br.readLine());
+
 		StringTokenizer st;
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			while(st.hasMoreTokens()) pq.add(Integer.parseInt(st.nextToken()));
+
+			while(st.hasMoreTokens()) {	// 입력 수를 전부 우선순위 큐에 저장함으로서 자동 정렬 가능
+				pq.add(Integer.parseInt(st.nextToken()));
+			}
 		}
-		for(int i=0; i<N-1; i++) pq.poll();
-		System.out.println(pq.peek());
+
+		int ans = 0;
+		for(int i=0; i<N; i++) {	// N번째 큰 수 찾기
+			ans = pq.poll();
+		}
+
+		System.out.println(ans);
 		br.close();
 	}
 }
